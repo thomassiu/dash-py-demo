14de15a4e1188da8f4051fcbea072d91b97d7a61
Thomas Siu <thomas.siu@gmail.com>
2022-02-15 13:16:27 -0800

    modify code

--- src/app.py
+++ src/app.py
@@ -1,25 +1,47 @@
-from dash import Dash, dcc, html, Input, Output
-import os
+from dash import Dash, html, dcc, Input, Output
+import altair as alt
+from vega_datasets import data
 
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+# Read in global data
+iris = data.iris()
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+# Setup app and layout/frontend
+app = Dash(
+    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+)
 
 server = app.server
 
 app.layout = html.Div(
     [
-        html.H2("Hello World"),
-        dcc.Dropdown(["LA", "NYC", "MTL"], "LA", id="dropdown"),
-        html.Div(id="display-value"),
+        html.Iframe(
+            id="scatter",
+            style={"border-width": "0", "width": "100%", "height": "400px"},
+        ),
+        dcc.Dropdown(
+            id="ycol-widget",
+            value="sepalLength",
+            options=[{"label": col, "value": col} for col in iris.columns],
+        ),
     ]
 )
 
-
-@app.callback(Output("display-value", "children"), [Input("dropdown", "value")])
-def display_value(value):
-    return f"You have selected {value}"
+# Set up callbacks/backend
+@app.callback(Output("scatter", "srcDoc"), Input("ycol-widget", "value"))
+def plot_altair(ycol):
+    chart = (
+        alt.Chart(iris)
+        .mark_point()
+        .encode(
+            y=ycol,
+            x="petalWidth",
+            tooltip="sepalLength",
+            color=alt.Color("species", scale=alt.Scale(scheme="dark2")),
+        )
+        .interactive()
+    )
+    return chart.to_html()
 
 
 if __name__ == "__main__":
